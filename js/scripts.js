43d08dc6a68740a8002c73ae7ca436012a8566a8
Alex Skreen, Alondra Warren and Dusty McCord <dusty@>
2020-01-21 16:20:51 -0800

    add first string into array. created new array with 3+ char words. Reversed it. Created new string

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-
+    var threeOrMore = [];
+    var weirdSentence;
+    
+    $('#submitButton').click(function(event){
+        event.preventDefault();
+        threeOrMore = []
+        var input = $('input#word-input').val();
+        var wordArr = input.split(" ");
+        console.log(wordArr);
+        wordArr.forEach(function(word){
+            if (word.length >= 3){
+                threeOrMore.push(word);
+            }
+        });
+        weirdSentence = threeOrMore.reverse().join(" ");
+        $('#list').text(weirdSentence);
+        console.log(threeOrMore);
+        console.log(weirdSentence);
+        
+    });
 });
